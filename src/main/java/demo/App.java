ffbe74ec4c0e1137ee3d601aa4dcd0241748ca19
Ravina <ravina.shingade@unboxsocial.com>
2023-09-17 01:14:10 +0530

    HyperLinks Count

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -8,16 +8,21 @@ import java.net.MalformedURLException;
 public class App {
     public void getGreeting() throws InterruptedException, MalformedURLException {
         //TestCases tests = new TestCases(); // Initialize your test class
-        AmazonSearch amazon = new AmazonSearch();
+        //AmazonSearch amazon = new AmazonSearch();
+        HyperlinkCount hyperLink = new HyperlinkCount();
 
         //Todo: call your test case functions one after other here
 
         //tests.testCase01();
-        amazon.testCase01();
-        
+        //amazon.testCase01();
+        hyperLink.testCase01();
+
         //END Tests
-        //tests.endTest(); // End your test by clearning connections and closing browser
-        amazon.endTest();
+        //End your test by clearning connections and closing browser
+
+        //tests.endTest(); 
+        //amazon.endTest();
+        hyperLink.endTest();
     }
 
     public static void main(String[] args) throws InterruptedException, MalformedURLException {
---
+++ src/main/java/demo/HyperlinkCount.java
@@ -0,0 +1,45 @@
+package demo;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HyperlinkCount
+{
+    ChromeDriver driver;
+    public HyperlinkCount()
+    {
+        System.out.println("Constructor: TestCases");
+        WebDriverManager.chromedriver().timeout(30).setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    public void endTest()
+    {
+        System.out.println("End Test: TestCases");
+        driver.close();
+        driver.quit();
+    }
+
+    public void testCase01()
+    {
+        //Launch browser  and Enter URL  https://in.bookmyshow.com/explore/home/chennai
+        driver.get("https://in.bookmyshow.com/explore/home/chennai");
+
+        //Locate the anchor tag and store in list Using Locator "Tag Name" <a>
+        List<WebElement> hyperLinks = driver.findElements(By.tagName("a"));
+                    
+        //Get and store the size of list in an integer variable count  <a>
+        int linkCount = hyperLinks.size();
+
+        //Print the count  
+        System.out.println("Count of hyperlinks : "+linkCount);
+    }
+}
