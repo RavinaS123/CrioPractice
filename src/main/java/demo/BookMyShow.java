c1a2f5900d619511ccde5dbacb5c32959122e03a
Ravina <ravina.shingade@unboxsocial.com>
2023-09-22 00:42:53 +0530

    Nested Frames

--- src/main/java/demo/BookMyShow.java
+++ src/main/java/demo/BookMyShow.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BookMyShow
 {
     ChromeDriver driver;
@@ -34,33 +33,50 @@ public class BookMyShow
 
     public void testCase01()
     {
+        //Launch browser with url  https://in.bookmyshow.com/explore/home/chennai
+        driver.get("https://in.bookmyshow.com/explore/home/chennai");
+
         try
         {
+            //Make a list of movies name displayed in first section Using Locator "Class" Name eJQlCo
         	List<WebElement> movieTitles1 = driver.findElements(By.className("eJQlCo"));
             
-            //right arrow
+            //Click on right arrow Using Locator "Class" Name fQuapp
             driver.findElement(By.className("fQuapp")).click();
             Thread.sleep(1000);
             
+            //Make a list of movies name displayed in second section Using Locator "Class" Name daKrZU
             List<WebElement> movieTitles2 = driver.findElements(By.className("daKrZU"));
             
-            //left arrow
+            //Click on left arrow Using Locator "Class" Name emCCuK
             driver.findElement(By.className("emCCuK")).click();
             Thread.sleep(1000);
             
+            //Merge both lists into a single list  
             List<WebElement> moviesTitle = new ArrayList<>();
-            for(WebElement webElement : movieTitles1)
-            {
-    			moviesTitle.add(webElement);
-    		}
-            for(WebElement webElement : movieTitles2)
-            {
-    			moviesTitle.add(webElement);
-    		}
+            moviesTitle.addAll(movieTitles1);
+            moviesTitle.addAll(movieTitles2);
             
+            /*Iterate through the final list to get each webelement  Using Locator "XPath" //img[contains(@alt,'"+title+"')]
+            Store the src attribute value in string variable  
+            Print the string variable */ 
             for (int i = 0; i < moviesTitle.size(); i++)
             {
-            	if(i < 5)
+                if(i == 5)
+            	{
+            		driver.findElement(By.className("fQuapp")).click();
+                    Thread.sleep(1000);
+            	}
+                WebElement movieTitleElement = moviesTitle.get(i);
+                String title = movieTitleElement.getText();
+
+                // Find the image element using alt attribute
+                WebElement imgURL = driver.findElement(By.xpath("//img[contains(@alt,'" + title + "')]"));
+                String imageURL = imgURL.getAttribute("src");
+
+                System.out.println(i + " : " + imageURL);
+
+            	/*if(i < 5)
             	{
             		String title = moviesTitle.get(i).getText();
                 	WebElement imgURL = driver.findElement(By.xpath("//img[contains(@alt,'"+title+"')]"));
@@ -81,7 +97,7 @@ public class BookMyShow
                     	String imageURL = imgURL.getAttribute("src");
                     	System.out.println(i+" : "+imageURL);
             		}
-            	}
+            	}*/
             }
             
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(100));
@@ -90,15 +106,18 @@ public class BookMyShow
             WebElement parent = driver.findElement(By.xpath("//*[@id=\"super-container\"]/div[2]/div[3]/div[1]/div[6]/div/div"));
             //WebElement section = parent.findElement(By.xpath("//div/div/div/div[2]/div/div"));
             
+            //Locate the webelement for title in premiere Using Locator "XPath" //a[contains(@class,'fHgWnO')][2]/div/div[3]/div[1]
             String title = parent.findElement(By.xpath("//a[contains(@class,'fHgWnO')][2]/div/div[3]/div[1]")).getText();
             System.out.println("Name : "+title);
+
+            //Locate the webelement for language in premiere Using Locator "XPath" //a[contains(@class,'fHgWnO')][2]/div/div[3]/div[2]
             String language = parent.findElement(By.xpath("//a[contains(@class,'fHgWnO')][2]/div/div[3]/div[2]")).getText();
             System.out.println("Name : "+title+"  Language : "+language);
             driver.quit();
         }
         catch(Exception e)
         {
-        	System.out.println(e);
+        	e.printStackTrace();
         }
     }
 }
--- src/main/java/demo/Linkedin.java
+++ src/main/java/demo/Linkedin.java
@@ -7,7 +7,6 @@ import java.util.Base64;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 //import net.bytebuddy.implementation.bytecode.ShiftLeft;
